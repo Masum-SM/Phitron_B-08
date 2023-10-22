90cd93004ac17569e1af2972fd8c7505326e3ee1
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-10-23 00:48:18 +0600

    Update A_Create_A_New_String.c

--- Codeforces/A_Create_A_New_String.c
+++ Codeforces/A_Create_A_New_String.c
@@ -1,1 +1,25 @@
+/* 
+    |-----------------------------------------|
+    |   Md.Unus Masum                         |
+    |   Competitive Programmer                |
+    |   GitHub : https://github.com/Masum-SM  |
+    |-----------------------------------------|
+*/
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
 
+int main(){
+    char s1[1000],s2[1000],ans[1000];
+    scanf("%s",&s1);
+    scanf("%s",&s2);
+    int l1 = strlen(s1);
+    int l2 = strlen(s2);
+    printf("%d %d\n",l1,l2);
+ 
+    printf("%s %s",s1,s2);
+
+
+    return 0;
+}
