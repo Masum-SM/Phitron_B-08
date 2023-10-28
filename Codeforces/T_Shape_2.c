8c83394be7264efbcb1226fbb58ecbabcc13fe04
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-10-28 14:37:48 +0600

    Add files via upload

---
+++ Codeforces/T_Shape_2.c
@@ -0,0 +1,31 @@
+/* 
+    |-----------------------------------------|
+    |   Md.Unus Masum                         |
+    |   Competitive Programmer                |
+    |   GitHub : https://github.com/Masum-SM  |
+    |-----------------------------------------|
+*/
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int k = 1,s=n;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = s-1 ; j > 0 ; j--){
+            printf(" ");
+        }
+        for(int j = 0 ; j < k ; j++){
+            printf("*");
+        }
+        k=k+2;
+        s--;
+        printf("\n");
+    }
+
+
+    return 0;
+}
